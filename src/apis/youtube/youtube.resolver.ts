b4a6add1a9d2e5528dacd649492b46895d320831
Kyuwon Kwon <kkwon920@gmail.com>
2023-04-13 21:31:16 +0900

    Feat: views 조회 추가
    출력 값 ObjectType Youtube 생성 (youtube.dto), 12개 조회 및 키워드 고정 videoId를 통한 조회수 조회 API 개발

--- src/apis/youtube/youtube.resolver.ts
+++ src/apis/youtube/youtube.resolver.ts
@@ -1,17 +1,14 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { YoutubeService } from './youtube.service';
+import { Youtube } from './dto/youtube.dto';
 
 @Resolver()
 export class YoutubeResolver {
   constructor(
     private readonly youtubeService: YoutubeService, //
   ) {}
-  // @Query(() => [{ videoUrl: String, thumbnailUrl: String }])
-  @Query(() => [String])
-  fetchYoutube(
-    @Args('keyword') keyword: string, //
-  ): Promise<string[]> {
-    //리턴 타입지정
-    return this.youtubeService.searchVideos({ keyword });
+  @Query(() => [Youtube])
+  fetchYoutube(): Promise<Youtube[]> {
+    return this.youtubeService.searchVideos();
   }
 }
