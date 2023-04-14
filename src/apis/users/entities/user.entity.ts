e44d9d1f3bd8b5e424462a604630110074485363
sounwoo <mna800@naver.com>
2023-04-14 18:46:16 +0900

    Feat: createUser 기능구현

---
+++ src/apis/users/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  @Field(() => String)
+  email: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  @Field(() => String)
+  nickName: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  @Field(() => String, { nullable: true })
+  intro: string;
+
+  @Column({ type: 'text', nullable: true })
+  @Field(() => String, { nullable: true })
+  picture: string;
+}
---
+++ src/apis/users/users.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/user-create.input';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+
+  @Mutation(() => User)
+  createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput, //
+  ): Promise<User> {
+    return this.usersService.createUser({ createUserInput });
+  }
+}
