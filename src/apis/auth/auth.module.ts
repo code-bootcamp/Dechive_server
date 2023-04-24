f6853c2e4769f173235698e0789963ca80924a3b
sounwoo <mna800@naver.com>
2023-04-24 13:58:37 +0900

    Fix : socialLogin 로직 수정

--- src/apis/auth/auth.module.ts
+++ src/apis/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersMoulde } from '../users/users.module';
+import { AuthController } from './auth.controller';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh-strategy';
+import { JwtGoogleStrategy } from './strategies/jwt-social-google.strategy';
+import { JwtKakaoStrategy } from './strategies/jwt-social-kakao.strategy';
+import { JwtNaverStrategy } from './strategies/jwt-social-naver.strategy';
 
 @Module({
   imports: [
@@ -14,6 +18,12 @@ import { JwtRefreshStrategy } from './strategies/jwt-refresh-strategy';
     AuthResolver, //
     AuthService,
     JwtRefreshStrategy,
+    JwtGoogleStrategy,
+    JwtKakaoStrategy,
+    JwtNaverStrategy,
+  ],
+  controllers: [
+    AuthController, //
   ],
 })
 export class AuthModule {}
