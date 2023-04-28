451747806b6ca20b9ce8a2b997303a3e513dec8c
sounwoo <mna800@naver.com>
2023-04-28 17:36:04 +0900

    Style : findOneEmail 로직 변경

--- src/apis/users/dto/authEamil.input.ts
+++ src/apis/users/dto/authEamil.input.ts
@@ -1,8 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 @InputType()
 export class AuthEmailInput {
+  @IsEmail()
   @Field(() => String)
   email: User['email'];
 
