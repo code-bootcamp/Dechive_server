e48533703d9118b409fc94378b774e5c58ffbae4
Kyuwon Kwon <kkwon920@gmail.com>
2023-04-29 17:53:07 +0900

    Fix: getOpenGraph 예외 처리

--- src/commons/util/getOpenGraph.ts
+++ src/commons/util/getOpenGraph.ts
@@ -1,9 +1,11 @@
+import { HttpException } from '@nestjs/common';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import { parse } from 'parse-open-graph';
 
 export async function getOpenGraph({ url }) {
-  const html = (await getHTML({ url })).data;
+  const getResult = await getHTML({ url });
+  const html = getResult.data;
   const $ = cheerio.load(html);
   const meta = $('meta[property]')
     .map((i, el) => ({
@@ -14,28 +16,32 @@ export async function getOpenGraph({ url }) {
   const result = parse(meta);
   const { title, description } = result.og;
   const imageUrl = result.og.image[0].url;
-  url = result.og.url;
+  if (result.og.url) url = result.og.url;
   return {
-    name: title,
+    name: title ? title : '',
     url,
-    imageUrl,
-    description,
+    imageUrl: imageUrl ? imageUrl : '',
+    description: description ? description : '',
   };
 }
 const getHTML = ({ url }) => {
-  return axios.get(url, {
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-      'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
-      // 'Host': 'www.coupang.com',
-      // 'Connection': 'keep-alive',
-      // 'Pragma': 'no-cache',
-      // Accept:
-      //   'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-      // 'Accept-Encoding': 'gzip, deflate, br',
-      // 'Upgrade-Insecure-Requests': '1',
-      // 'Cache-Control': 'no-cache',
-    },
-  });
+  return axios
+    .get(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
+        // 'Host': 'www.coupang.com',
+        // 'Connection': 'keep-alive',
+        // 'Pragma': 'no-cache',
+        // Accept:
+        //   'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        // 'Accept-Encoding': 'gzip, deflate, br',
+        // 'Upgrade-Insecure-Requests': '1',
+        // 'Cache-Control': 'no-cache',
+      },
+    })
+    .catch(() => {
+      throw new HttpException('잘못된 주소입니다', 401);
+    });
 };
