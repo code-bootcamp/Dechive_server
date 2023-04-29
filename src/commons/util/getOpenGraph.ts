e19ea6e303ade7c6687580058a2a0f4ee3e68286
Kyuwon Kwon <kkwon920@gmail.com>
2023-04-29 17:25:59 +0900

    Fix: getOpenGraph Coupang 오류 해결

--- src/commons/util/getOpenGraph.ts
+++ src/commons/util/getOpenGraph.ts
@@ -1,25 +1,41 @@
-import { HttpException } from '@nestjs/common';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { parse } from 'parse-open-graph';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ogs = require('open-graph-scraper');
-
-export function getOpenGraph({ url }) {
-  const options = {
+export async function getOpenGraph({ url }) {
+  const html = (await getHTML({ url })).data;
+  const $ = cheerio.load(html);
+  const meta = $('meta[property]')
+    .map((i, el) => ({
+      property: $(el).attr('property'),
+      content: $(el).attr('content'),
+    }))
+    .get();
+  const result = parse(meta);
+  const { title, description } = result.og;
+  const imageUrl = result.og.image[0].url;
+  url = result.og.url;
+  return {
+    name: title,
     url,
-    onlyGetOpenGraphInfo: true,
-    timeout: { request: 500 },
+    imageUrl,
+    description,
   };
-  return ogs(options)
-    .then((data) => {
-      const { error, result } = data;
-      console.log(error);
-      return error
-        ? null
-        : {
-            name: result.ogTitle,
-            imageUrl: result.ogImage.url,
-            description: result.ogDescription,
-          };
-    })
-    .catch(() => null);
 }
+const getHTML = ({ url }) => {
+  return axios.get(url, {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+      'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',
+      // 'Host': 'www.coupang.com',
+      // 'Connection': 'keep-alive',
+      // 'Pragma': 'no-cache',
+      // Accept:
+      //   'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      // 'Accept-Encoding': 'gzip, deflate, br',
+      // 'Upgrade-Insecure-Requests': '1',
+      // 'Cache-Control': 'no-cache',
+    },
+  });
+};
