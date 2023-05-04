27ae33bc8726fb09346532b3916d1de57cb12b7f
Kyuwon Kwon <kkwon920@gmail.com>
2023-05-04 19:49:39 +0900

    Fix: access multiple CORS callback function 사용

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,19 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(graphqlUploadExpress());
   app.enableCors({
-    origin: process.env.WHITELIST.split(', '),
+    origin: function (origin, callback) {
+      if (!origin || process.env.WHITELIST.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   await app.listen(5000, () => {
     console.log('=================');
+    console.log(process.env.WHITELIST.split(', '));
+    console.log(process.env.WHITELIST);
     console.log('🐶🐶🐶 graphql 백엔드 서버 오픈 🐶🐶🐶');
     console.log('=================');
   });
