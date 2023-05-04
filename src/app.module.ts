165e867ab32536cd77132e5ba823fdbe51125f63
Kyuwon Kwon <kkwon920@gmail.com>
2023-05-04 20:16:22 +0900

    Fix: CORS test app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,13 +36,13 @@ import { ProductsModule } from './apis/products/products.module';
       useFactory: () => ({
         autoSchemaFile: 'src/commons/graphql/schema.gql',
         context: ({ req, res }) => ({ req, res }),
-        // cors: {
-        //   origin: [
-        //     'http://localhost:3000',
-        //     'https://client-web-dechive.vercel.app/',
-        //   ],
-        //   credentials: true,
-        // },
+        cors: {
+          origin: [
+            'http://localhost:3000',
+            'https://client-web-dechive.vercel.app/',
+          ],
+          credentials: true,
+        },
       }),
     }),
     TypeOrmModule.forRoot({
