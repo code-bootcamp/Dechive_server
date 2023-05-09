5f5c6e67d8d3d69978fb0d1114b3948ef764867b
Kyuwon Kwon <kkwon920@gmail.com>
2023-05-09 19:06:52 +0900

    Feat: multiple Cors 14

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -63,7 +63,7 @@ export class AuthService {
     if (process.env.ORIGIN === 'http://localhost:3000')
       res.setHeader(
         'Set-Cookie',
-        `refreshToken=${refreshToken}; path=/; domain=http://34.64.190.109:5000; Secure=false; SameSite=None; httpOnly`,
+        `refreshToken=${refreshToken}; path=/; Secure=false; SameSite=None; httpOnly`,
       );
     else {
       res.setHeader(
